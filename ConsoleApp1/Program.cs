7fa90913f4dd0b066865083732f7cc9f28dea2a8
zbino <740090794>
2019-10-11 20:46:29 +0800

    new2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("输出信息");
+            Console.WriteLine("");
             Console.ReadLine();
 
 
